351604c92ec19dba10258a2fcb8ae6b64601feeb
Roland-Siyapze <119140740+Roland-Siyapze@users.noreply.github.com>
2023-11-29 16:29:50 +0100

    Commit

---
+++ Quiz Game JS/question.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "The elements between the and tags of an HTML table are ______ by default.",
+    answer: "Left aligned",
+    options: [
+      "Justified",
+      "Left aligned",
+      "Centrally aligned",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the role of the Controller in MVC architecture?",
+    answer: "It acts as an intermediary between Model and View, handling user inputs and updates.",
+    options: [
+      "It interfaces with the database.",
+      "It structures the data in a meaningful way for the application. ",
+      "It acts as an intermediary between Model and View, handling user inputs and updates.",
+      "It defines how data should be presented to the user."
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which programming language is used for web development?",
+    answer: "JavaScript",
+    options: [
+      "Java",
+      "Python",
+      "JavaScript",
+      "C++"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the purpose of the <head> tag in HTML?",
+    answer: "To contain meta-information about the document",
+    options: [
+      "To define the main content of the document",
+      "To contain meta-information about the document",
+      "To define a section of the document",
+      "To define a hyperlink"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the correct way to comment in JavaScript?",
+    answer: "// This is a comment",
+    options: [
+      "// This is a comment",
+      "<-- This is a comment -->",
+      "/* This is a comment */",
+      "## This is a comment"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Which CSS property is used to change the text color?",
+    answer: "color",
+    options: [
+      "background-color",
+      "text-color",
+      "font-color",
+      "color"
+    ]
+  },
+    {
+    numb: 7,
+    question: "What does API stand for?",
+    answer: "Application Programming Interface",
+    options: [
+      "Application Programming Interface",
+      "Application Program Interface",
+      "Application Protocol Interface",
+      "Application Process Interface"
+    ]
+  },
+    {
+    numb: 8,
+    question: "What is the correct way to select an element with the id 'myElement' in JavaScript?",
+    answer: "document.getElementById('myElement')",
+    options: [
+      "document.getElementByClassName('myElement')",
+      "document.getElementById('myElement')",
+      "document.querySelector('#myElement')",
+      "document.getElementByTagName('myElement')"
+    ]
+  },
+    {
+    numb: 9,
+    question: "Which HTML tag is used to create a hyperlink?",
+    answer: "<a>",
+    options: [
+      "&lt;a&gt",
+      "&lt;h&gt",
+      "&lt;p&gt",
+      "&lt;b&gt"
+    ]
+  },
+    {
+    numb: 10,
+    question: "What is the correct syntax for a function declaration in JavaScript?",
+    answer: "function myFunction() {}",
+    options: [
+      "function = myFunction() {}",
+      "function: myFunction() {}",
+      "function => myFunction() {}",
+      "function myFunction() {}"
+    ]
+  },
+];
